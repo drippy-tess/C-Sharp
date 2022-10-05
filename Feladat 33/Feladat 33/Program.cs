04c75a8cb02a05b54d15038b5c681cf8c86ac059
drippy_tesss <kovacshaziadam@gmail.com>
2022-10-05 12:13:39 +0200

    c#

---
+++ Feladat 33/Feladat 33/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Feladat_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] kar = new char[5];
+            int i;
+            int db = 0;
+
+            Console.WriteLine("Kérek 5 karaktert!");
+            for (i = 0; i < 5; i++)
+            {
+                kar[i] = Convert.ToChar(Console.ReadLine());
+                if (kar[i] == 'a' || kar[i] == 'A')
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine("A darabszám: {0}", db);
+        }
+    }
+}
---
+++ Feladat 34/Feladat 34/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Feladat_34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = new int[10];
+            int[] ps = new int[10];
+            int[] ptl = new int[10];
+            int index1 = 0;
+            int index2 = 0;
+
+            Console.WriteLine("Kérek 10 számot!");
+            for (int i = 0; i < 10; i++)
+            {
+                t[i] = Convert.ToInt32(Console.ReadLine());
+                if (t[i] % 2 == 0)
+                {
+                    ps[index1] = t[i];
+                    index1++;
+                }
+                else {
+                    ptl[index2] = t[i];
+                    index2++;
+                }
+            }
+            for (int j = 0; j < index1; j++)
+            {
+                Console.WriteLine(ps[j]);
+            }
+            Console.WriteLine("Páratlan elemek:");
+            for (int j = 0; j < index2; j++)
+            {
+                Console.WriteLine(ptl[j]);
+            }
+        }
+    }
+}
