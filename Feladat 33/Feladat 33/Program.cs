9908e39d0e9c34baf6d3a05b603fa75f508b8ed0
drippy_tesss <kovacshaziadam@gmail.com>
2022-10-05 12:22:12 +0200

    c#

--- Feladat 33/Feladat 33/Program.cs
+++ Feladat 33/Feladat 33/Program.cs
@@ -20,6 +20,14 @@ namespace Feladat_33
                 }
             }
             Console.WriteLine("A darabszám: {0}", db);
+            if (db > 0)
+            {
+                Console.WriteLine("Van a vizsgált karakter a tömbben.");
+            }
+            else
+            {
+                Console.WriteLine("Nincs a vizsgált karakter a tömbben.");
+            }
         }
     }
 }
