15365bee2e307a804631f4e15d3a5111b9514715
drippy_tesss <kovacshaziadam@gmail.com>
2022-10-07 14:08:48 +0200

    c#

---
+++ Feladat 35/Feladat 35/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Feladat_35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tombMerete = 0;
+            int[] tomb = new int[tombMerete];
+            Random vsz = new Random();
+            int i;
+
+            Console.Write("Kérem adja meg a tömb elemeinek számát: ");
+            tombMerete = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i <= tomb.Length; i++)
+            {
+                tomb[i] = vsz.Next(-200, 200);
+            }
+            Console.WriteLine("A tömb elemei: {0}", tomb);
+
+        }
+    }
+}
---
+++ Feladat 36/Feladat 36/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Feladat_36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int[] tomb = new int[7];
+            int atlag = 0;
+
+            Console.WriteLine("Kérem adja meg a kiadásait hétfőtől péntekig:");
+
+            for (i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = Convert.ToInt32(Console.ReadLine());
+                atlag = atlag + tomb[i];
+                if (tomb[i] < 10000)
+                {
+                    Console.WriteLine("Volt 10000Ft-nál kisebb kiadás.");
+                }
+            }
+            atlag = atlag / 7;
+            Console.WriteLine("Az átlag: {0}", atlag);
+
+        }
+    }
+}
