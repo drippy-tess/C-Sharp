c2155fc6353fa8fc012703e2f363d1e3636a527a
drippy_tesss <kovacshaziadam@gmail.com>
2022-10-07 14:17:01 +0200

    c#

--- Feladat 35/Feladat 35/Program.cs
+++ Feladat 35/Feladat 35/Program.cs
@@ -6,19 +6,35 @@ namespace Feladat_35
     {
         static void Main(string[] args)
         {
-            int tombMerete = 0;
+            int tombMerete = Convert.ToInt32(Console.ReadLine());
             int[] tomb = new int[tombMerete];
             Random vsz = new Random();
             int i;
+            int parosSzamok = 0;
+            int paratlanSzamok = 0;
+            int atlag = 0;
 
-            Console.Write("Kérem adja meg a tömb elemeinek számát: ");
-            tombMerete = Convert.ToInt32(Console.ReadLine());
-
-            for (i = 0; i <= tomb.Length; i++)
+            for (i = 0; i < tomb.Length; i++)
             {
                 tomb[i] = vsz.Next(-200, 200);
+                atlag = atlag + tomb[i];
+                if (tomb[i] % 5 == 0)
+                {
+                    Console.WriteLine("Volt 0-ra, vagy 5-r végződő szám");
+                }
+                if (tomb[i] % 2 == 0)
+                {
+                    parosSzamok += 1;
+                }
+                else
+                {
+                    paratlanSzamok += 1;
+                }
+                
             }
-            Console.WriteLine("A tömb elemei: {0}", tomb);
+            Console.WriteLine("A tömb elemei: {0}", tomb[i]);
+            atlag = atlag / tombMerete;
+            Console.WriteLine("Tömb átlaga: {0}, páros számok: {1}, páratlan számok{2}", atlag, parosSzamok, paratlanSzamok);
 
         }
     }
