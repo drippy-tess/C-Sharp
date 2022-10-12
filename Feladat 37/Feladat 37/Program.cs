2584e7a32cc4f91ec77408f2ce0d67174f06114c
drippy_tesss <kovacshaziadam@gmail.com>
2022-10-12 12:09:06 +0200

    c#

---
+++ Feladat 37/Feladat 37/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Feladat_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            double atlag;
+            int dbszam = 0;
+            double osszeg = 0;
+            double szam;
+            szam = Convert.ToDouble(Console.ReadLine());
+            osszeg += szam;
+            dbszam++;
+
+            while (szam != 0)
+            {
+                szam = Convert.ToDouble(Console.ReadLine());
+                osszeg += szam;
+                dbszam++;
+            }
+            atlag = osszeg / (dbszam -1);
+            Console.WriteLine("Az átlag: {0}", Math.Round(atlag, 2));
+        }
+    }
+}
