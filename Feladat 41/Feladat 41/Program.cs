a88aab8da10dc2cc979d3f0f7ceb046089866259
drippy_tesss <kovacshaziadam@gmail.com>
2022-10-12 12:09:06 +0200

    c#

--- Feladat 41/Feladat 41/Program.cs
+++ Feladat 41/Feladat 41/Program.cs
@@ -6,7 +6,32 @@ namespace Feladat_41
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int kezdo;
+            int veg;
+            int csere;
+            int db = 0;
+            int i;
+
+            Console.Write("Kérem az intervallum kezdőértékét:");
+            kezdo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérem az intervallum végértékét:");
+            veg = Convert.ToInt32(Console.ReadLine());
+            if(kezdo > veg)
+            {
+                csere = kezdo;
+                kezdo = veg;
+                veg = csere;
+            }
+            i = kezdo;
+            while(i <= veg)
+            {
+                if(i % 3 == 0)
+                { 
+                db++;
+                }
+                i++;
+            }
+            Console.WriteLine("A hárommal oszthatóak dadabszáma:{0}", db);
         }
     }
 }
