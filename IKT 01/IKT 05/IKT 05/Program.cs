a26b9ef671a4e48b2bc76a7b5688636116f2d38f
drippy_tess <74898218+drippy-tess@users.noreply.github.com>
2022-10-24 09:55:47 +0200

    c#

---
+++ IKT 01/IKT 05/IKT 05/Program.cs
@@ -0,0 +1,131 @@
+﻿float[] elsoUgras = new float[5];
+float[] masodikUgras = new float[5];
+float[] harmadikUgras = new float[5];
+float[] ugrasOsszeg = new float[5];
+Random vsz = new Random();
+int ervenytelen = 0;
+int elsoIndex = 0;
+int masodikIndex = 0;
+int harmadikIndex = 0;
+int negyedikIndex = 0;
+int otodikIndex = 0;
+int kiestek = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    elsoUgras[i] = vsz.Next(0,5) + vsz.NextSingle();
+    masodikUgras[i] = vsz.Next(0, 5) + vsz.NextSingle();
+    harmadikUgras[i] = vsz.Next(0, 5) + vsz.NextSingle();
+}
+
+for (int j = 0; j < 5; j++)
+{
+    if (elsoUgras[j] == 0)
+    {
+        ervenytelen++;
+        switch (j)
+        {
+            case 0:
+                elsoIndex++;
+                break;
+            case 1:
+                masodikIndex++;
+                break;
+            case 2:
+                harmadikIndex++;
+                break;
+            case 3:
+                negyedikIndex++;
+                break;
+            default:
+                otodikIndex++;
+                break;
+        }
+
+    }
+    if (masodikUgras[j] == 0)
+    {
+        ervenytelen++;
+        switch (j)
+        {
+            case 0:
+                elsoIndex++;
+                break;
+            case 1:
+                masodikIndex++;
+                break;
+            case 2:
+                harmadikIndex++;
+                break;
+            case 3:
+                negyedikIndex++;
+                break;
+            default:
+                otodikIndex++;
+                break;
+        }
+    }
+    if (harmadikUgras[j] == 0)
+    {
+        ervenytelen++;
+        switch (j)
+        {
+            case 0:
+                elsoIndex++;
+                break;
+            case 1:
+                masodikIndex++;
+                break;
+            case 2:
+                harmadikIndex++;
+                break;
+            case 3:
+                negyedikIndex++;
+                break;
+            default:
+                otodikIndex++;
+                break;
+        }
+    }
+}
+
+if(elsoIndex > 1)
+{
+    kiestek++;
+}
+if (masodikIndex > 1)
+{
+    kiestek++;
+}
+if (harmadikIndex > 1)
+{
+    kiestek++;
+}
+if (negyedikIndex > 1)
+{
+    kiestek++;
+}
+if (otodikIndex > 1)
+{
+    kiestek++;
+}
+
+for (int l = 0; l < 5; l++)
+{
+    ugrasOsszeg[l] = elsoUgras[l] + masodikUgras[l] + harmadikUgras[l];
+}
+
+for (int k = 4; k > 0; k--)
+{
+    for (int m = 0; m < k; m++)
+    {
+        if (ugrasOsszeg[m] > ugrasOsszeg[m + 1])
+        {
+            float temp = ugrasOsszeg[m];
+            ugrasOsszeg[m] = ugrasOsszeg[m + 1];
+            ugrasOsszeg[m + 1] = temp;
+        }
+    }
+}
+
+Console.Write($"{ervenytelen/15*100}%-ban voltak sikertelen ugrások\nA versenyből kiestek: {kiestek}\nElső helyezett ugrásai: {ugrasOsszeg[4]}");
---
+++ IKT 01/IKT 06/IKT 06/Program.cs
@@ -0,0 +1,66 @@
+﻿int[] elsoJatekos = new int[3];
+int[] masodikJatekos = new int[3];
+int[] harmadikJatekos = new int[3];
+int[] negyedikJatekos = new int[3];
+Random vsz = new Random();
+float legnagyobbAtlag = 0;
+
+for (int i = 0; i < 3; i++)
+{
+    elsoJatekos[i] = vsz.Next(1, 100);
+    masodikJatekos[i] = vsz.Next(1, 100);
+    harmadikJatekos[i] = vsz.Next(1, 100);
+    negyedikJatekos[i] = vsz.Next(1, 100);
+}
+for (int k = 2; k > 0; k--)
+{
+    for (int m = 0; m < k; m++)
+    {
+    if (elsoJatekos[m] > elsoJatekos[m + 1])
+    {
+        int temp = elsoJatekos[m];
+        masodikJatekos[m] = masodikJatekos[m + 1];
+        masodikJatekos[m + 1] = temp;
+    }
+    }
+}
+
+for (int k = 2; k > 0; k--)
+{
+    for (int m = 0; m < k; m++)
+    {
+        if (elsoJatekos[m] > elsoJatekos[m + 1])
+        {
+            int temp = elsoJatekos[m];
+            elsoJatekos[m] = elsoJatekos[m + 1];
+            elsoJatekos[m + 1] = temp;
+        }
+        if (masodikJatekos[m] > masodikJatekos[m + 1])
+        {
+            int temp = elsoJatekos[m];
+            masodikJatekos[m] = masodikJatekos[m + 1];
+            masodikJatekos[m + 1] = temp;
+        }
+        if (harmadikJatekos[m] > harmadikJatekos[m + 1])
+        {
+            int temp = elsoJatekos[m];
+            harmadikJatekos[m] = harmadikJatekos[m + 1];
+            harmadikJatekos[m + 1] = temp;
+        }
+        if (negyedikJatekos[m] > negyedikJatekos[m + 1])
+        {
+            int temp = elsoJatekos[m];
+            negyedikJatekos[m] = negyedikJatekos[m + 1];
+            negyedikJatekos[m + 1] = temp;
+        }
+    }
+}
+
+float[] legjobb = new float[4] { elsoJatekos[2], masodikJatekos[2], harmadikJatekos[2], negyedikJatekos[2]};
+
+for (int j = 0; j < 4; j++)
+{
+    legjobb[j] += legnagyobbAtlag;
+}
+
+Console.WriteLine($"A legnagyobb számok átlaga: {legnagyobbAtlag / 5}");
