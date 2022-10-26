b47806c7dafadbdd9da9b3b8d00fd290dc633e9c
drippy_tess <74898218+drippy-tess@users.noreply.github.com>
2022-10-26 12:18:16 +0200

    c#

---
+++ Feladat 48/Feladat 48/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Feladat_48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem dobjon kétszer: ");
+            int elsoDobas = Convert.ToInt32(Console.ReadLine());
+            int masodikDobas = Convert.ToInt32(Console.ReadLine());
+            int osszes = 0;
+            switch(elsoDobas)
+            {
+                case int n when (n == 1):
+                    osszes += 1000;
+                    break;
+                case int n when (n == 2):
+                    osszes -= 1000;
+                    break;
+                case int n when (n == 3):
+                    osszes += 500;
+                    break;
+                case int n when (n == 4):
+                    osszes -= 4000;
+                    break;
+                case int n when (n == 5):
+                    osszes += 5000;
+                    break;
+                case int n when (n == 6):
+                    osszes += 10000;
+                    break;
+                default:
+                    break;
+            }
+
+            switch (masodikDobas)
+            {
+                case int n when (n == 1):
+                    osszes += 1000;
+                    break;
+                case int n when (n == 2):
+                    osszes -= 1000;
+                    break;
+                case int n when (n == 3):
+                    osszes += 500;
+                    break;
+                case int n when (n == 4):
+                    osszes -= 4000;
+                    break;
+                case int n when (n == 5):
+                    osszes += 5000;
+                    break;
+                case int n when (n == 6):
+                    osszes += 10000;
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine($"Összesen {osszes}Ft-ot nyert.");
+        }
+    }
+}
---
+++ Feladat 49/Feladat 49/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Feladat_49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérem adja meg a fizetését:");
+            int fizetes = Convert.ToInt32(Console.ReadLine());
+
+            switch(fizetes)
+            {
+                case int n when (n < 150000):
+                    Console.WriteLine("Túl alacsony.");
+                    break;
+                case int n when ((n < 180000) && (n >= 150000)):
+                    Console.WriteLine("Alacsony.");
+                    break;
+                case int n when ((n < 250000) && (n >= 180000)):
+                    Console.WriteLine("Megfelelő.");
+                    break;
+                case int n when (n >= 250000):
+                    Console.WriteLine("Kiváló.");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
