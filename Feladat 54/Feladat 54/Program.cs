2db026eb7d996b8d4db62b5e95111c1661fac892
drippy-tess <kovacshaziadam@gmail.com>
2022-11-11 08:14:54 +0100

    c#

---
+++ Feladat 54/Feladat 54/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Feladat_54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string szo;
+            Console.Write("Kérem adjon meg egy szót: ");
+            szo = Console.ReadLine();
+
+            for(int i = 0; i < szo.Length; i = i +=3)
+            {
+                Console.WriteLine($"{szo[i]}");
+            }
+        }
+    }
+}
