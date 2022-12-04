9f627fa756c0eb1dd1a8980606a5328736d18b52
drippy_tess <74898218+drippy-tess@users.noreply.github.com>
2022-12-04 12:48:53 +0100

    c#

---
+++ feladat 80/feladat 80/Program.cs
@@ -0,0 +1,12 @@
+﻿int[,] t = new int[3, 5];
+Random vsz = new Random();
+Console.WriteLine("Rendezett kiíratás: ");
+for (int i = 0; i < 3; i++)
+{
+    for(int k = 0; k < 5; k++)
+    {
+        t[i, k] = vsz.Next(1, 100);
+        Console.Write(t[i, k] + "\t"); 
+    }
+    Console.WriteLine();
+}
