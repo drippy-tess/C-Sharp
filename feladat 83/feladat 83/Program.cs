ac57726626c1369ff65040afcd2e3a6a50a65f91
drippy_tess <74898218+drippy-tess@users.noreply.github.com>
2022-12-04 12:43:44 +0100

    c#

---
+++ feladat 83/feladat 83/Program.cs
@@ -0,0 +1,20 @@
+﻿int[,] t = new int[4, 6];
+Random vsz = new Random();
+//int[] sorosszeg = new int[4];
+int[] sorosszeg = { 0, 0, 0, 0 };
+for (int i = 0; i < 4; i++)
+{
+    for(int k = 0; k < 6; k++)
+    {
+        t[i, k] = vsz.Next(100, 500);
+        sorosszeg[i] += t[i, k];
+        Console.Write(t[i, k] + "\t");
+    }
+    Console.WriteLine();
+}
+Array.Sort(sorosszeg);
+Console.WriteLine("A sorok összegei: ");
+for(int j = 0; j < 4; j++)
+{
+    Console.WriteLine(sorosszeg[j]);
+}
---
+++ feladat 84/feladat 84/Program.cs
@@ -0,0 +1,23 @@
+﻿int[,] t = new int[10, 10];
+int min = int.MaxValue;
+Random vsz = new Random();
+for (int i = 0; i < 10; i++)
+{
+    for(int k = 0; k < 10; k++)
+    {
+        t[i, k] = vsz.Next(50, 100);
+        Console.Write(t[i, k] + "\t");
+    }
+    Console.WriteLine();
+}
+for(int i = 0; i < 10; i++)
+{
+    for(int k = 0; k < 10; k++)
+    {
+        if (t[k, i] < min)
+        {
+            min = t[i, k];
+        }
+    }
+    Console.WriteLine("Minimum értékek: {0}", min + "\t");
+}
