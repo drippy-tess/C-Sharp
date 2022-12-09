7c2d38d4cdab1cbafffe0cb3a93cd932d994294c
drippy_tess <74898218+drippy-tess@users.noreply.github.com>
2022-12-09 14:11:49 +0100

    c#

---
+++ Feladat 70/Feladat 70/Program.cs
@@ -0,0 +1,25 @@
+﻿int szam = 0;
+int temp;
+
+while(szam == 0)
+{
+    Console.Write("Kérem adjon meg egy háromjegyű számot: ");
+    temp = Convert.ToInt32(Console.ReadLine());
+    if(temp > 99 && temp <= 999)
+    {
+        szam = temp;
+    }
+    else
+    {
+        Console.WriteLine("helytelen bemenet!");
+    }
+}
+
+if(szam % 7 == 0)
+{
+    Console.WriteLine("A megadott szám osztható héttel.");
+}
+else
+{
+    Console.WriteLine("A megadott szám nem osztható héttel.");
+}
---
+++ Feladat 71/Feladat 71/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int szam = 1;
+int negativ = 0;
+int pozitiv = 0;
+
+while(szam != 0)
+{
+    Console.Write("Kérem adjon meg egy számot: ");
+    szam = Convert.ToInt32(Console.ReadLine());
+    if (szam < 0)
+    {
+        negativ++;
+    }
+
+    else
+    {
+        pozitiv++;
+    }
+}
+
+Console.WriteLine($"A megadott szám elérte a nullát!\n{negativ}db negatív számot, és {pozitiv}db pozitív számot adott meg.");
