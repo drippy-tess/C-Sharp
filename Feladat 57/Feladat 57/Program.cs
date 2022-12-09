679901d491143533a929b093d2eba833beb6e4a7
drippy_tess <74898218+drippy-tess@users.noreply.github.com>
2022-12-09 14:13:43 +0100

    c#

---
+++ Feladat 57/Feladat 57/Program.cs
@@ -0,0 +1,16 @@
+﻿string mondat;
+Console.Write("Kérem gépeljen be egy mondatot: ");
+mondat = Console.ReadLine();
+
+string[] tordelt = mondat.Split(' ');
+int max = tordelt[0].Length;
+
+for (int i = 1; i < tordelt.Length; i++)
+{
+    if (tordelt[i].Length > max)
+    {
+        max = tordelt[i].Length;
+    }
+}
+
+Console.WriteLine($"A leghosszabb szó hossza: {max} karakter.");
---
+++ Feladat 58/Feladat 58/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Kérem adjon meg egy nevet: ");
+string nev = Console.ReadLine();
+string[] kettos = { "Cs", "Dz", "Gy", "Sz", "Zs" };
+string[] nevek = nev.Split(' ');
+string monogram = nevek[0].Substring(0, 1);
+if (kettos.Contains(nevek[1].Substring(0, 2)))
+{
+    monogram += nevek[1].Substring(0, 2);
+}
+else
+{
+    monogram += nevek[1].Substring(0, 1); 
+}
+
+Console.WriteLine($"A monogram: {monogram}");
