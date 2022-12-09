0e481c214e891c5f35065e78c0da6a5c6308fddf
drippy_tess <74898218+drippy-tess@users.noreply.github.com>
2022-12-09 14:12:52 +0100

    c#

---
+++ Feladat 59/Feladat 59/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Kérem gépeljen be mondatokat: ");
+string mondatok = Console.ReadLine();
+int mondatSzam = 0;
+
+for(int i = 0; i < mondatok.Length; i++)
+{
+    if ((mondatok[i]) == '.' || (mondatok[i] == '!') || (mondatok[i] == '?'))
+    {
+        mondatSzam++;
+    }
+}
+
+Console.WriteLine($"A mondatok száma: {mondatSzam}db");
---
+++ Feladat 60/Feladat 60/Program.cs
@@ -0,0 +1,17 @@
+﻿int kijelentodarab = 0;
+int kerdojelDarab = 0;
+int felszolitoDarab = 0;
+Console.Write("Kérem adjon meg egy mondatot: ");
+string mondat = Console.ReadLine();
+
+for(int i = 0; i < mondat.Length; i++)
+{
+    switch(mondat[i])
+    {
+        case '.': kijelentodarab++; break;
+        case '!': felszolitoDarab++; break;
+        case '?': kerdojelDarab++; break;
+    }
+}
+
+Console.WriteLine($"Kijelentő mondatok száma: {kijelentodarab}\nFelszólító mondatok száma: {felszolitoDarab}\nKérdő mondatok száma: {kerdojelDarab}");
